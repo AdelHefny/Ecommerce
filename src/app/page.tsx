1865eb1171465afad5ccb6c031fa42c844cfed22
Adel Hefny <adelahmedhefny@gmail.com>
2023-11-05 11:29:11 +0200

    quick fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="">
       <FrontPage />
-      <ProductsComponent />
+      <ProductsComponent children={<></>} />
       <Service />
     </main>
   );
